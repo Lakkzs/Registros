76dd977041d062371b233d6c1b8590be9fa0ecde
Lakkzs <hectordz0004@gmail.com>
2023-12-28 17:30:27 -0600

    Paginación dinámica - Ya no se usa el #each dentro del hbs de los números para la paginación, ahora se envian directamente los últimos 3 números a los últimos 3 botones. Dentro del controlador se agregó la variable para guardar la página máxima de los registros, además si se detecta que el botón presionado es el último entra al if para sumarle una cantidad a los siguientes números y que los últimos 3 tengan valores diferentes, además esto funciona de manera que si se le da clic al penultimo no se suma de más, dentro de esta validación tenemos otra validación que nos permite mantener los mismos últimos tres números una vez la última página sea visible en el paginador.

--- controllers/ctrl_tabla.js
+++ controllers/ctrl_tabla.js
@@ -6,22 +6,31 @@ module.exports = {
         if(Object.keys(req.query).length != 0){
             console.log(1, 'asd', parseInt(req.query.p))
             let resultado = await db.loadTable.loadTableData(req.query)
-            console.log(150, resultado)
+            let Max = resultado.datos[1][0].Paginas
             if(parseInt(req.query.p) >= 5){
                 console.log(777)
-                let cuarto = parseInt(req.query.p)
-                let quinto = parseInt(req.query.p)+1
-                let sexto = parseInt(req.query.p)+2
-                res.render('tabla/tabla', {tabla: resultado.datos[0], paginas: resultado.datos[1], Cuarto: cuarto, Quinto: quinto, Sexto: sexto})
+                let cuarto = parseInt(req.query.p)+1
+                let quinto = parseInt(req.query.p)+2
+                let sexto = parseInt(req.query.p)+3
+                if(Max == cuarto){
+                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: cuarto-2, Quinto: quinto-2, Sexto: sexto-2})
+                }else if(Max == quinto){
+                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: cuarto-1, Quinto: quinto-1, Sexto: sexto-1})
+                }else if(Max == sexto){
+                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: cuarto, Quinto: quinto, Sexto: sexto})
+                }else{
+                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: cuarto, Quinto: quinto, Sexto: sexto})
+                }
             }else{
                 console.log(888)
-                res.render('tabla/tabla', {tabla: resultado.datos[0], paginas: resultado.datos[1], Cuarto: 4, Quinto: 5, Sexto: 6})
+                res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: 4, Quinto: 5, Sexto: 6})
             }
         }else{
             console.log(0, 'dsa')
             let resultado = await db.loadTable.loadTableData({p: 0, c: 20})
-            console.log(150, resultado)
-            res.render('tabla/tabla', {tabla: resultado.datos[0], paginas: resultado.datos[1], Cuarto: 4, Quinto: 5, Sexto: 6})
+            let Max = resultado.datos[1][0].Paginas
+            console.log(150, resultado.datos[1][0].Paginas)
+            res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: 4, Quinto: 5, Sexto: 6})
         }
     },
 
