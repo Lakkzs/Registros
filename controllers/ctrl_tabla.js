1e385f8b42e50e9ea1c9a139dc89aabef8183123
Lakkzs <hectordz0004@gmail.com>
2023-12-28 16:02:19 -0600

    Gráfica de altas por mes - Agregado una función para traer el número de altas que se tuvieron en cada mes del año. Dentro del controlador de sección se añadieron las variables de igual forma para pasarlas directamente sin usar un #each. Las variables se ingresan en el hbs de seccion y mediante un getelementById son registradas en variables dentro del script de seccion para poderlas usar dentro de la gráfica.

--- controllers/ctrl_tabla.js
+++ controllers/ctrl_tabla.js
@@ -4,15 +4,24 @@ module.exports = {
     tabla: async (req, res) => {
         console.log(100000000000000000, req.query)
         if(Object.keys(req.query).length != 0){
-            console.log(1, 'asd')
+            console.log(1, 'asd', parseInt(req.query.p))
             let resultado = await db.loadTable.loadTableData(req.query)
             console.log(150, resultado)
-            res.render('tabla/tabla', {tabla: resultado.datos[0], paginas: resultado.datos[1]})
+            if(parseInt(req.query.p) >= 5){
+                console.log(777)
+                let cuarto = parseInt(req.query.p)
+                let quinto = parseInt(req.query.p)+1
+                let sexto = parseInt(req.query.p)+2
+                res.render('tabla/tabla', {tabla: resultado.datos[0], paginas: resultado.datos[1], Cuarto: cuarto, Quinto: quinto, Sexto: sexto})
+            }else{
+                console.log(888)
+                res.render('tabla/tabla', {tabla: resultado.datos[0], paginas: resultado.datos[1], Cuarto: 4, Quinto: 5, Sexto: 6})
+            }
         }else{
             console.log(0, 'dsa')
             let resultado = await db.loadTable.loadTableData({p: 0, c: 20})
             console.log(150, resultado)
-            res.render('tabla/tabla', {tabla: resultado.datos[0], paginas: resultado.datos[1]})
+            res.render('tabla/tabla', {tabla: resultado.datos[0], paginas: resultado.datos[1], Cuarto: 4, Quinto: 5, Sexto: 6})
         }
     },
 
