9e5542c3eeffcb84f5313d5c7d70472086d42f36
Lakkzs <hectordz0004@gmail.com>
2023-12-29 10:06:00 -0600

    Paginación dinámica - Se agrega la funcionalidad de solo mostrar las páginas necesarias en caso de que estas sean menores a 6, para así evitar tener botones de más que no nos lleven a ningún sitio.

--- controllers/ctrl_tabla.js
+++ controllers/ctrl_tabla.js
@@ -7,23 +7,45 @@ module.exports = {
             console.log(1, 'asd', parseInt(req.query.p))
             let resultado = await db.loadTable.loadTableData(req.query)
             let Max = resultado.datos[1][0].Paginas
-            if(parseInt(req.query.p) >= 5){
-                console.log(777)
-                let cuarto = parseInt(req.query.p)+1
-                let quinto = parseInt(req.query.p)+2
-                let sexto = parseInt(req.query.p)+3
-                if(Max == cuarto){
-                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: cuarto-2, Quinto: quinto-2, Sexto: sexto-2})
-                }else if(Max == quinto){
-                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: cuarto-1, Quinto: quinto-1, Sexto: sexto-1})
-                }else if(Max == sexto){
-                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: cuarto, Quinto: quinto, Sexto: sexto})
+            console.log('Max', Max)
+            if(Max == 1){
+                console.log('unoooooooo')
+                res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true})
+            }if(Max == 2){
+                console.log('dossssssss')
+                res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true})
+            }if(Max == 3){
+                console.log('tresssssss')
+                res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true})
+            }if(Max == 4){
+                console.log('cuatrooooo')
+                res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true, Cuat: true, Cuarto: 4,})
+            }if(Max == 5){
+                console.log('cincoooooo')
+                res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true, Cuat: true, Cinc: true, Cuarto: 4, Quinto: 5})
+            }if(Max == 6){
+                console.log('seisssssss')
+                res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true, Cuat: true, Cinc: true, Seis: true})
+            }
+            else{
+                if(parseInt(req.query.p) >= 5){
+                    console.log(777)
+                    let cuarto = parseInt(req.query.p)+1
+                    let quinto = parseInt(req.query.p)+2
+                    let sexto = parseInt(req.query.p)+3
+                    if(Max == cuarto){
+                        res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true, Cuat: true, Cinc: true, Seis: true, Cuarto: cuarto-2, Quinto: quinto-2, Sexto: sexto-2})
+                    }else if(Max == quinto){
+                        res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true, Cuat: true, Cinc: true, Seis: true, Cuarto: cuarto-1, Quinto: quinto-1, Sexto: sexto-1})
+                    }else if(Max == sexto){
+                        res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true, Cuat: true, Cinc: true, Seis: true, Cuarto: cuarto, Quinto: quinto, Sexto: sexto})
+                    }else{
+                        res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true, Cuat: true, Cinc: true, Seis: true, Cuarto: cuarto, Quinto: quinto, Sexto: sexto})
+                    }
                 }else{
-                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: cuarto, Quinto: quinto, Sexto: sexto})
+                    console.log(888)
+                    res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Prim: true, Segs: true, Terc: true, Cuat: true, Cinc: true, Seis: true, Cuarto: 4, Quinto: 5, Sexto: 6})
                 }
-            }else{
-                console.log(888)
-                res.render('tabla/tabla', {tabla: resultado.datos[0], Paginas: Max, Cuarto: 4, Quinto: 5, Sexto: 6})
             }
         }else{
             console.log(0, 'dsa')
