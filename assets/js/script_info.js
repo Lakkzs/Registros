f65e21367dc3fad667ffe5c567391aa15eeb72f5
Camila Lineth <a182564@une.edu.mx>
2024-01-05 13:24:06 -0600

    INFO ADICIONAL,  INFO CONTACTOS EMERGENCIA- se hicieron las pantallas yaun no hacen un insert

--- assets/js/script_info.js
+++ assets/js/script_info.js
@@ -1,48 +1,75 @@
-
-function onRegistro(e){
-    e.preventDefault()
-
-    var formInfo = new FormData(document.getElementById('formInfo'))
-    var jsonInfo = {}
-    var arrFaltantes = []
-    var validacion = 0
-    for(var key of formInfo.keys()){
-        jsonInfo[key] = formInfo.get(key)
-        console.log(jsonInfo[key])
-        if((jsonInfo[key] == '' || jsonInfo[key] == null || jsonInfo[key] == undefined  || jsonInfo[key] == 'Nada' )){
-            arrFaltantes.push(' ' + key.replace("txt", ""))
-        }
-    }
-    console.log(arrFaltantes);
-    if(validacion == 1){
-        if(arrFaltantes.length > 0){
-            alert(`Por favor llene los siguientes campos faltantes:${arrFaltantes}`)
-        }
-        // else{
-        //     fetch('/rt_recuperar', {
-        //         method: 'POST',
-        //         headers: {'Content-Type': 'application/json'},
-        //         body: JSON.stringify(jsonInfo)
-        //     }).then((response) => response.json())
-        //     .then((response) => {
-        //         console.log(response)
-        //         if(response.resp.datos[0].RESULT == 'INCORRECTO'){
-        //             alert('El correo ingresado no está registrado, intente de nuevo.')
-        //         }
-        //         if(response.resp.datos[0].RESULT == 'ACTUALIZADO'){
-        //             alert('La recuperación se ha realizado correctamente, se le ha enviado un correo con su nueva contraseña.')
-        //             document.getElementById('formInfo').submit();
-        //         }
-        //     })
-        //     .catch(function (err) {
-        //         console.log(err)
-        //     })
-        // }
-    }
-    else{
-        if(arrFaltantes.length > 0){
-            alert(`Por favor llene los siguientes campos faltantes:${arrFaltantes}`)
-        }
+function otro(value) {
+    if (value == "Otro") {
+        document.getElementById('oculto').removeAttribute('hidden');
+    } else {
+        document.getElementById('oculto').setAttribute('hidden', 'hidden');
     }
 }
 
+function otro2(value) {
+    if (value == "Otro") {
+        document.getElementById('oculto2').removeAttribute('hidden');
+    } else {
+        document.getElementById('oculto2').setAttribute('hidden', 'hidden');
+    }
+}
+
+function otro3(value) {
+    if (value == "Otro") {
+        document.getElementById('oculto3').removeAttribute('hidden');
+    } else {
+        document.getElementById('oculto3').setAttribute('hidden', 'hidden');
+    }
+}
+
+function dependencia(value) {
+    console.log(value, typeof (value))
+    if (value == 1) {
+        document.getElementById('numero').removeAttribute('disabled');
+    } else {
+        document.getElementById('numero').setAttribute('disabled', 'disabled');
+    }
+}
+
+function onRegistro(e) {
+    e.preventDefault()
+
+    fetch('/rt_infoAdicional', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(jsonRegistroPuestos)
+    }).then((response) => response.json())
+    .then((response) => {
+        console.log(response)
+        if(response.datos[0].RESULT == 'EXISTE'){
+        }
+        if(response.datos[0].RESULT == 'NO EXISTE'){
+            alert('EL COLABORADR AUN NO CUENTA CON DATOS INGRESADOS')
+        }
+    })
+    .catch(function (err) {
+        console.log(err)
+    })
+
+}
+
+function onRegistro2(e) {
+    e.preventDefault()
+
+    var formInfo = new FormData(document.getElementById('formInfoAdicional'))
+    var jsonInfo = {}
+    var arrFaltantes = []
+
+    for (var key of formInfo.keys()) {
+        jsonInfo[key] = formInfo.get(key)
+        console.log(jsonInfo[key])
+        if ((jsonInfo[key] == '' || jsonInfo[key] == null || jsonInfo[key] == undefined || jsonInfo[key] == 'Nada')) {
+            arrFaltantes.push(' ' + key.replace("txt", ""))
+        }
+    }
+
+    if (arrFaltantes.length > 0) {
+        alert(`Por favor llene los siguientes campos faltantes:${arrFaltantes}`)
+    }
+
+}
--- controllers/ctrl_info.js
+++ controllers/ctrl_info.js
@@ -3,16 +3,30 @@ module.exports = {
     info: (req, res) => {
         res.render('info/info')
     },
-    infoAdicional: (req, res) => {
-        res.render('infoColaborador/info_Adicional')
+    infoAdicional: async (req, res) => {
+        let resultado = await db.altas.cargaColaboradores()
+        res.render('infoColaborador/info_Adicional',{colaboradores: resultado.datos})
     },
     infoEstudios: (req, res) => {
         res.render('infoColaborador/info_Estudios')
     },
-    infoEmergencia: (req, res) => {
-        res.render('infoColaborador/info_Emergencia')
+    infoEmergencia: async(req, res) => {
+        let resultado = await db.altas.cargaColaboradores()
+        res.render('infoColaborador/info_Emergencia',{colaboradores: resultado.datos})
     },
     infoSalud: (req, res) => {
         res.render('infoColaborador/info_Salud')
     },
+    rt_infoAdicional: async(req,res) => {
+        try{
+            console.log(777,req)
+            let body = req.body
+            let datos = (await db.altas.altaPuestos(body)).datos
+            console.log(datos)
+            res.json({status: 'OK', datos})
+        } catch(error){
+            console.log(error)
+            res.json({estatus:'ERROR'})
+        }
+    }
 }
