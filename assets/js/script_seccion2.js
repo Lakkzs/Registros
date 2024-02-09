112dbf1c6d6b2bdb8ae3eaf798746143390f4b82
Lakkzs <hectordz0004@gmail.com>
2024-02-09 09:45:11 -0600

    Calendario - Habia un error que hacia que los colores del los eventos del calendario se quitaron y se pusiera el color negro, esto ha sido corregido para siempre mostrar estos colores despues de realizar la seleccion del evento.

--- assets/js/script_seccion2.js
+++ assets/js/script_seccion2.js
@@ -20,34 +20,59 @@ window.onload = async function carga(){
             eventsData: 
                 datosDias[0]
             ,
-            dateChanged: (currentDate, events) => {
-                console.log("date change", currentDate, events);
-                if(events[0]){
-                    alert(events[0].name + ' (' + events[0].CATEGORIA_DIAS_FESTIVOS + ')')
-                }
-            },
-            monthChanged: (currentDate, events) => {
-                if(events.length > 0){
+            monthChanged: async (currentDate, events) => {
+                eventosM = []
+                console.log(777, events)
+                if (events.length > 0) {
                     console.log("month change", currentDate, events);
                     let evento = document.getElementsByClassName('calendar__day-event')
                     console.log(evento)
                     for (let i = 0; i < events.length; i++) {
                         if (events[i].CATEGORIA_DIAS_FESTIVOS == "Día festivo oficial") {
                             evento[i].children[1].setAttribute('style', 'background-color: #d1272b')
-                        }else if (events[i].CATEGORIA_DIAS_FESTIVOS == "Día festivo no oficial"){
+                        } else if (events[i].CATEGORIA_DIAS_FESTIVOS == "Día festivo no oficial") {
                             evento[i].children[1].setAttribute('style', 'background-color: #f4b31b')
-                        }else if (events[i].CATEGORIA_DIAS_FESTIVOS == "Evento de empresa"){
+                        } else if (events[i].CATEGORIA_DIAS_FESTIVOS == "Evento de empresa") {
                             evento[i].children[1].setAttribute('style', 'background-color: #60a709')
                         }
                     }
                 }
-            }
+                eventosM.push(events)
+            },
+            dateChanged: async (currentDate, events) => {
+                console.log("date change", currentDate, events);
+                if (events[0]) {
+                    alert(events[0].name + ' (' + events[0].CATEGORIA_DIAS_FESTIVOS + ')')
+                }
+                let evento = document.getElementsByClassName('calendar__day-event')
+                console.log(321, evento)
+                console.log(123, events)
+                eventosM.flat(1)
+                await colorChange(eventosM)
+            },
         });
     })
     .catch(function (err) {
         console.log(err)
     })
   }
+
+  async function colorChange(eventosM) {
+    if (eventosM.length > 0) {
+        console.log(eventosM[0])
+        let evento = document.getElementsByClassName('calendar__day-event')
+        console.log(evento)
+        for (let i = 0; i < eventosM[0].length; i++) {
+            if (eventosM[0][i].CATEGORIA_DIAS_FESTIVOS == "Día festivo oficial") {
+                evento[i].children[1].setAttribute('style', 'background-color: #d1272b')
+            } else if (eventosM[0].CATEGORIA_DIAS_FESTIVOS == "Día festivo no oficial") {
+                evento[i].children[1].setAttribute('style', 'background-color: #f4b31b')
+            } else if (eventosM[0].CATEGORIA_DIAS_FESTIVOS == "Evento de empresa") {
+                evento[i].children[1].setAttribute('style', 'background-color: #60a709')
+            }
+        }
+    }
+}
     
 
 
